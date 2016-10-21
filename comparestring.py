5b8fe3a5f2052df197681152833da6f486201c3a
root <root@localhost.localdomain>
2016-10-21 09:46:54 -0700

    compare string

---
+++ comparestring.py
@@ -0,0 +1,13 @@
+#! /usr/var/envy python
+
+a = 'belajar python'
+s = 'belajar perl'
+pecah1 = set(a.split(' '))
+pecah2 = set(s.split(' '))
+print pecah1 == pecah2
+
+b = 'TOS'
+n = 'TOS'
+banding1 = set(b.split(' '))
+banding2 = set(n.split(' '))
+print banding1 == banding2
